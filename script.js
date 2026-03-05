a102f38fdb48d3c4759bfcb9c146a09e9f3ad48d
IngJefersonGil <Ing.jefersongil@gmail.com>
2026-03-05 18:41:46 -0500

    ...

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 
     // Toggle del menú responsive
+    const menuToggle = document.querySelector(".menu-toggle");
     const navLinks = document.querySelector(".nav-links");
 
     if (menuToggle && navLinks) {
